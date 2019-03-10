bddf4f8b9427a82e486243ead161b6b66f8c5a86
hoalex <svt75@hotmail.com>
2019-03-10 17:55:35 +0100

    Interface has been created for translation.

---
+++ AmericanDream/Controller/TranslationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TranslationViewController.swift
+//  AmericanDream
+//
+//  Created by Alex on 10/03/2019.
+//  Copyright © 2019 Alexandre Holet. All rights reserved.
+//
+
+import UIKit
+
+class TranslationViewController: UIViewController {
+
+    @IBOutlet weak var textToTranslate: UITextField!
+    @IBOutlet weak var translatedLabel: UILabel!
+    @IBOutlet weak var translateButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
