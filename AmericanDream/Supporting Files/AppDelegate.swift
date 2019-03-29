6d046ae2b63ea49b46387de656d0ada0848daf28
hoalex <svt75@hotmail.com>
2019-03-29 22:03:39 +0100

    3D touch shortcuts have been added to the app.

--- AmericanDream/Supporting Files/AppDelegate.swift
+++ AmericanDream/Supporting Files/AppDelegate.swift
@@ -19,28 +19,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        guard let type = Bundle.main.bundleIdentifier else {
+            return
+        }
+        let shortcutType = shortcutItem.type
+        let root = window?.rootViewController as! UITabBarController
+        
+        switch shortcutType {
+        case type + ".ChangeRate":
+            root.selectedIndex = 0
+        case type + ".Translate":
+            root.selectedIndex = 1
+        case type + ".Weather" :
+            root.selectedIndex = 2
+        default:
+            break
+        }
+        completionHandler(false)
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
